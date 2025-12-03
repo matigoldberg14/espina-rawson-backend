1876b1c85f44f13b81770e5d041640a4feaf2e48
matigoldberg14 <matigoldberg3@gmail.com>
2025-12-02 21:22:29 -0300

    aas

--- src/controllers/auction.controller.ts
+++ src/controllers/auction.controller.ts
@@ -168,6 +168,8 @@ export class AuctionController {
         secondaryImage5,
       };
       let processedPdfUrl = pdfUrl;
+      let processedPdfData: Buffer | undefined;
+      let processedPdfFilename: string | undefined;
 
       if (files && files.length > 0) {
         try {
